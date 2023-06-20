1574eb77a3ef6bfaf928b0d88c724b449bf9fa5f
Николай Захариев <80208943+NickZach2036@users.noreply.github.com>
2023-06-20 20:22:52 +0300

    My contribution to the code

---
+++ QuestionAddMenu.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
+
+#include "File_crypting.c"
+
+// Returns 0 if adding is successfull; 
+// Exits with -1 if it cannot create/write in file;
+// Returns -2 if wrong keys were used;
+int addQuestion () {
+    // the keys must be 0 - 32; now the correct keys are: 17, 10, 20, 6
+    int key_0, key_1, key_2, key_3;
+    
+    do {
+        printf("Input keys [k0 k1 k2 k3]: ");
+        scanf("%d %d %d %d", &key_0, &key_1, &key_2, &key_3); // now the correct keys are: 17, 10, 20, 6
+    } while (load_file(key_0, key_1, key_2, key_3) == -2); // while the used keys are wrong
+
+    // printf("\nThe file was loaded correctly!\n\n");
+
+    int diff;
+    char name[MAX_TEXT_LENGTH];
+    char correct[MAX_TEXT_LENGTH];
+    char wrongOne[MAX_TEXT_LENGTH];
+    char wrongTwo[MAX_TEXT_LENGTH];
+    char wrongThree[MAX_TEXT_LENGTH];
+
+    printf("Input difficulty: ");
+    scanf ("%d", &diff);
+    getchar();
+    printf("Input question: ");
+    scanf("%[^\n]", name);
+    getchar();
+    printf("Input correct answer: ");
+    scanf("%[^\n]", correct);
+    getchar();
+    printf("Input wrong answer 1: ");
+    scanf("%[^\n]", wrongOne);
+    getchar();
+    printf("Input wrong answer 2: ");
+    scanf("%[^\n]", wrongTwo);
+    getchar();
+    printf("Input wrong answer 3: ");
+    scanf("%[^\n]", wrongThree);
+    getchar();
+
+    struct Question q = init_question(diff, name, correct, wrongOne, wrongTwo, wrongThree);
+
+    return add_question_in_file(q, key_0, key_1, key_2, key_3);
+}
+
+int main () {
+    int flag; // Comment variable.
+
+    addQuestion();
+
+    struct Question q = init_question(0, "", "", "", "", "");
+
+    if((flag = get_question_from_file(&q, 3, 17, 10, 20, 6)) == -2) printf ("You used wrong keys!");
+    else if (flag == -5) printf("There is no question with this difficulty!");
+    else if (flag == 0) printf ("%d %s %s %s %s %s", q.difficulty, q.name, q.correct_ans, q.wrong_ans_1, q.wrong_ans_2, q.wrong_ans_3);
+    
+    return 0;
+}
